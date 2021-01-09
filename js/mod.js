d9f328fe8b08f5a07ed638baf40991c237beeef2
Glitch (wary-guttural-pedestrian) <none>
2021-01-09 02:06:22 +0000

    🏇🚤 Checkpoint ./js/mod.js:9783767/95

--- js/mod.js
+++ js/mod.js
@@ -17,9 +17,9 @@ let VERSION = {
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+	<h3>v0.1</h3><br>
+		- Added 2 layers (the 3rd layer is not done yet).<br>
+		- bottom text`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
